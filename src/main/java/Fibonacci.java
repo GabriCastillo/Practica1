a1f5f43f0cfa03daaa54ceee7d9d1fd19911068a
Gabriel Castillo <castillogabriel@uma.es>
2022-03-06 14:44:04 +0100

    Creation of javaTest
    Se han creado los tests unitarios para la clase Fibonacci

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,22 +1,23 @@
 public class Fibonacci {
+    /*
+        En matemáticas, la sucesión o serie de Fibonacci es la siguiente sucesión infinita de números naturales:
+            0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597...
+        La sucesión comienza con los números 0 y 1; a partir de estos, «cada término es la suma de los dos anteriores», es la relación de recurrencia que la define.
 
-    //En matemáticas, la sucesión o serie de Fibonacci es la siguiente sucesión infinita de números naturales:
-        //0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597...
-    //La sucesión comienza con los números 0 y 1; a partir de estos, «cada término es la suma de los dos anteriores», es la relación de recurrencia que la define.
-
-    public int compute(int fibo){//Fibo se refiere al termino "n" de la sucesion.
-        int result=0;
+     */
+    public int compute(int fibo) {//Fibo se refiere al termino "n" de la sucesion.
+        int result = 0;
 
         if (fibo < 0) {
             throw new RuntimeException("numero negativo");//comentario
         }
 
-        if(fibo==0){
+        if (fibo == 0) {
             return 0;
-        }else if(fibo == 1){
+        } else if (fibo == 1) {
             return 1;
-        }else {
-            result = compute(fibo-1) + compute(fibo-2);
+        } else {
+            result = compute(fibo - 1) + compute(fibo - 2);
         }
         return result;
     }
--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -1,5 +1,74 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+/*
+    Los números de Fibonacci quedan definidos por las ecuaciones
+
+            Fibonacci {0} -> 0
+            Fibonacci {1} -> 1
+            Fibonacci {2} -> 1
+            Fibonacci {3} -> 2
+            Fibonacci {4} -> 3
+            Fibonacci {n} -> Fibonacci {n-1} + Fibonacci {n-2}
+ */
 
 class FibonacciTest {
 
+    @Test
+    public void testComputeReturnZeroIfTheNumberIsZero() {
+        Fibonacci fibonacci = new Fibonacci();
+
+        int expectedValue = 0;
+        int obtainValue = fibonacci.compute(0);
+
+        assertEquals(expectedValue, obtainValue);
+    }
+
+    @Test
+    public void testComputeReturnOneIfTheNumberIsOne(){
+        Fibonacci fibonacci = new Fibonacci();
+
+        int expectedValue = 1;
+        int obtainValue = fibonacci.compute(1);
+
+        assertEquals(expectedValue, obtainValue);
+    }
+
+    @Test
+    public void testComputeReturnOneIfTheNumberIsTwo(){
+        Fibonacci fibonacci = new Fibonacci();
+
+        int expectedValue = 1;
+        int obtainValue = fibonacci.compute(2);
+
+        assertEquals(expectedValue, obtainValue);
+    }
+
+    @Test
+    public void testComputeReturnTwoIfTheNumberIsThree(){
+        Fibonacci fibonacci = new Fibonacci();
+
+        int expectedValue = 2;
+        int obtainValue = fibonacci.compute(3);
+
+        assertEquals(expectedValue, obtainValue);
+    }
+
+    @Test
+    public void testComputeReturnThreeIfTheNumberIsFour(){
+        Fibonacci fibonacci = new Fibonacci();
+
+        int expectedValue = 3;
+        int obtainValue = fibonacci.compute(4);
+
+        assertEquals(expectedValue, obtainValue);
+    }
+
+    @Test
+    public void testComputeThroesExceptionIfTheNumberIsNegative(){
+        var fibonacci = new Fibonacci();
+        assertThrows(RuntimeException.class, () -> fibonacci.compute(-1)) ;
+
+    }
 }
